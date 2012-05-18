c35ea60b7134adf0eea9444aad44a998c11b69c3
Durran Jordan <durran@gmail.com>
2012-05-18 16:43:48 +0200

    Can move imports around

--- src/test/scala/org/speckle/SpecSpec.scala
+++ src/test/scala/org/speckle/SpecSpec.scala
@@ -1,7 +1,8 @@
-import org.speckle.{ Matchers, Spec }
+import org.speckle.Matchers
+import org.speckle.Macros._
+import org.speckle.Spec
 
 class SpecSpec extends Spec with Matchers {
-  import org.speckle.Macros._
 
   describe("#describe")
 }
